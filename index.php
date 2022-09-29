087ad1b3070af62ae1d490b38156d873e9ab558e
Adam Fawwaz <fawwazadam1005@gmail.com>
2022-09-29 13:59:01 +0700

    keduax

--- index.php
+++ index.php
@@ -5,9 +5,82 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Perpustakaan 5 jaya</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="slick/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick/slick-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="WOW-master/css/libs/animate.css"/>
   </head>
   <body>
-    <h1>PERPUSTAKAAN 5</h1>
+      <nav class="navbar navbar-expand-lg bg-secondary bg-opacity-10 py-2">
+        <div class="container">
+          <a class="navbar-brand" href="#"><i class="fa fa-coffee"></i>Perpustakaan 5 Jaya</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link text-warning" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link bg-warning text-white rounded-pill" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+      <!-- isi 1 -->
+
+      <div class="container mt-5 pt-5">
+        <div class="row">
+            <div class="col-md-7">
+                <h1 class="wow fadeInUp">Hi ! I am</h1>
+                <h1 class="text-warning wow fadeInUp">Adam Fawwaz</h1>
+                <p class="wow fadeInUp">Welcome to Adam Fawwaz We want our monthly roundup of new relases</p>
+                <div class="ps-5 mt-4">
+                    <button class="btn bg-warning rounded-pill text-white shadow ps-4 pe-4"> HIRE </button>
+                </div>
+            </div>
+            <div class="col-md-5 wow fadeInRight">
+                <img src="img/bg1.png" alt="">
+                <img src="img/samurai cat.png" alt="" width="90%"  style="margin-top: -350px; margin-left: 50px;">
+            </div>
+        </div>
+      </div>
+
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="slick/slick/slick.min.js"></script>
+    <script type="text/javascript" src="WOW-master/dist/wow.js"></script>
+  <script>
+    $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+  </script>
+  <script>
+    wow = new WOW(
+                      {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       0,          // default
+                      mobile:       true,       // default
+                      live:         true        // default
+                    }
+                    )
+                    wow.init();
+  </script>
   </body>
 </html>
